b90427e4ffbda404895423ec6031a3200b27f052
aslezar <97354675+aslezar@users.noreply.github.com>
2024-05-19 14:42:27 +0530

    updated user router

--- controllers/user.ts
+++ controllers/user.ts
@@ -48,16 +48,15 @@ const updateUser = async (
 }
 
 const updateCompleteProfile = async (req: Request, res: Response) => {
-    const { name, bio, myInterests } = req.body
+    const { name, phoneNo } = req.body
     const userId = req.user.userId
 
-    if (!name || !bio || !myInterests)
-        throw new BadRequestError("Name, Bio or Interests are required")
+    if (!name || !phoneNo)
+        throw new BadRequestError("Name or Phone Number are required")
 
     const user = await User.findByIdAndUpdate(userId, {
         name,
-        bio,
-        myInterests,
+        phoneNo,
     })
 
     res.status(StatusCodes.OK).json({
@@ -82,6 +81,7 @@ const updateProfileImage = async (req: Request, res: Response) => {
         msg: "Image Updated Successfully",
     })
 }
+
 const deleteProfileImage = async (req: Request, res: Response) => {
     const userId = req.user.userId
 
@@ -102,137 +102,5 @@ const deleteProfileImage = async (req: Request, res: Response) => {
         msg: "Image Deleted Successfully",
     })
 }
-const getAllAssets = async (req: Request, res: Response) => {
-    const userId = req.user.userId
-    const user = await User.findById(userId).select("myAssests")
-    if (!user) throw new UnauthenticatedError("User Not Found")
 
-    res.status(StatusCodes.OK).json({
-        data: { assets: user.myAssests },
-        success: true,
-        msg: "All Assets Fetched Successfully",
-    })
-}
-
-const uploadAssets = async (req: Request, res: Response) => {
-    const userId = req.user.userId
-    const files = req.files
-    if (!files) throw new BadRequestError("Files are required")
-
-    //upload files to cloudinary
-    const cloudinary_img_urls = await cloudinaryUploadAssetsImages(req)
-
-    await User.findByIdAndUpdate(userId, {
-        $push: { myAssests: { $each: cloudinary_img_urls } },
-    })
-
-    res.status(StatusCodes.OK).json({
-        data: cloudinary_img_urls,
-        success: true,
-        msg: "Assets Uploaded Successfully",
-    })
-}
-
-const deleteAsset = async (req: Request, res: Response) => {
-    const userId = req.user.userId
-    const { assets } = req.body
-    if (!assets) throw new BadRequestError("Assets are required")
-
-    const public_id = assets
-        .split("/")
-        .slice(-3)
-        .join("/")
-        .split(".")
-        .slice(0, -1)
-        .join(".")
-
-    const userIdFromUrl = public_id.split("/")[1]
-
-    if (userIdFromUrl != userId)
-        throw new BadRequestError("You are not authorized to delete this asset")
-
-    //delete assets from cloudinary
-    const isDeleted: boolean = await cloudinaryDeleteAssetImages(public_id)
-    if (!isDeleted) throw new BadRequestError("Failed to delete assets")
-    await User.findByIdAndUpdate(userId, {
-        $pull: { myAssests: assets },
-    })
-
-    res.status(StatusCodes.OK).json({
-        success: true,
-        msg: "Assets Deleted Successfully",
-    })
-}
-
-const followUnfollowUser = async (req: Request, res: Response) => {
-    const userId = req.user.userId
-    const { followId } = req.body
-
-    if (!followId) throw new BadRequestError("FollowId is required")
-
-    const user = await User.findById(userId)
-    if (!user) throw new UnauthenticatedError("User Not Found")
-
-    const followUser = await User.findById(followId)
-    if (!followUser) throw new BadRequestError("Follow User Not Found")
-
-    const isFollowing = user.following.includes(followId)
-    const isFollower = followUser.followers.includes(userId)
-
-    if (isFollowing && isFollower) {
-        await User.findByIdAndUpdate(userId, {
-            $pull: { following: followId },
-        })
-        await User.findByIdAndUpdate(followId, {
-            $pull: { followers: userId },
-        })
-    } else if (!isFollowing && !isFollower) {
-        await User.findByIdAndUpdate(userId, {
-            $push: { following: followId },
-        })
-        await User.findByIdAndUpdate(followId, {
-            $push: { followers: userId },
-        })
-    } else {
-        throw new BadRequestError("Something Went Wrong")
-    }
-
-    res.status(StatusCodes.OK).json({
-        success: true,
-        msg: "Follow/Unfollow User Successfully",
-    })
-}
-
-const isFollowing = async (req: Request, res: Response) => {
-    const userId = req.user.userId
-    const { followId } = req.body
-
-    if (!followId) throw new BadRequestError("FollowId is required")
-
-    const user = await User.findById(userId)
-    if (!user) throw new UnauthenticatedError("User Not Found")
-
-    const followUser = await User.findById(followId)
-    if (!followUser) throw new BadRequestError("Follow User Not Found")
-
-    const isFollowing = user.following.includes(followId)
-    const isFollower = followUser.followers.includes(userId)
-
-    res.status(StatusCodes.OK).json({
-        data: { isFollowing: isFollowing && isFollower },
-        success: true,
-        msg: "Check Following Successfully",
-    })
-}
-
-export {
-    getMe,
-    updateCompleteProfile,
-    updateProfileImage,
-    deleteProfileImage,
-    getAllAssets,
-    uploadAssets,
-    deleteAsset,
-    followUnfollowUser,
-    isFollowing,
-}
+export { getMe, updateCompleteProfile, updateProfileImage, deleteProfileImage }
--- routes/user.ts
+++ routes/user.ts
@@ -5,29 +5,17 @@ import {
     updateCompleteProfile,
     updateProfileImage,
     deleteProfileImage,
-    getAllAssets,
-    uploadAssets,
-    deleteAsset,
-    followUnfollowUser,
-    isFollowing,
 } from "../controllers/user"
-import userBlogRouter from "./userBlog"
+// import userBlogRouter from "./userBlog"
 
 const router = Router()
 
 router.route("/me").get(getMe)
-router.use("/blog", userBlogRouter)
+// router.use("/blog", userBlogRouter)
 router.patch("/update-profile", updateCompleteProfile)
 router
     .route("/image")
     .post(upload.single("profileImage"), updateProfileImage)
     .delete(deleteProfileImage)
-router
-    .route("/assets")
-    .get(getAllAssets)
-    .post(upload.array("assetFiles", 5), uploadAssets)
-    .delete(deleteAsset)
-router.post("/follow-unfollow", followUnfollowUser)
-router.post("/is-following", isFollowing)
 
 export default router
