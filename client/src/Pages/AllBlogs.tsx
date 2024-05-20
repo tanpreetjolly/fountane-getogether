fd55ec01f64c728f3813525e6910c7991ab736a3
tanpreetjolly <tanpreet2002@gmail.com>
2024-05-20 21:27:34 +0530

    removed unused code

--- client/src/Pages/AllBlogs.tsx
+++ client/src/Pages/AllBlogs.tsx
@@ -1,19 +1,9 @@
-import Blogs from "../components/Blogs"
-import Categories from "../components/Categories"
-import { useEffect, useState } from "react"
-import { TrendingType } from "../definitions"
-import { getTrendingBlog } from "../api"
-import { Link, useNavigate } from "react-router-dom"
-import Loader from "../components/Loader"
-import TrendingSvg from "../assets/img/Feed/TrendingSvg"
-import AuthorTag from "../components/AuthorTag"
+import { useNavigate } from "react-router-dom"
 import Button from "../components/Button"
 // add icon from react-icons
 import { FaPlus } from "react-icons/fa"
 import EventCard from "../components/EventCard"
 const AllBlogs = () => {
-  const [trending, setTrending] = useState<TrendingType[]>([])
-  const [loading, setLoading] = useState<boolean>(true)
   const navigate = useNavigate()
   // useEffect(() => {
   //   setLoading(true)
