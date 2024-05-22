125ed419da15a00bf8f1171c6aa57b3b1392aab0
aslezar <97354675+aslezar@users.noreply.github.com>
2024-05-22 15:07:12 +0530

    added vendor service search

--- controllers/search.ts
+++ controllers/search.ts
@@ -1,4 +1,5 @@
 import User from "../models/user"
+import Vendor from "../models/vendorProfile"
 import { Request, Response } from "express"
 import { BadRequestError } from "../errors"
 import { StatusCodes } from "http-status-codes"
@@ -29,6 +30,37 @@ const search = async (req: Request, res: Response) => {
                 },
                 success: true,
                 msg: "Users Fetched Successfully",
+            })
+        case "vendor":
+            const vendorTotalCount = await Vendor.countDocuments({
+                services: {
+                    $elemMatch: {
+                        serviceName: { $regex: query, $options: "i" },
+                    },
+                },
+            })
+
+            const vendors = await Vendor.find({
+                services: {
+                    $elemMatch: {
+                        serviceName: { $regex: query, $options: "i" },
+                    },
+                },
+            })
+                .select("userId services")
+                .skip(req.pagination.skip)
+                .limit(req.pagination.limit)
+                .sort({ createdAt: -1 })
+
+            return res.status(StatusCodes.OK).json({
+                data: {
+                    vendors,
+                    totalCount: vendorTotalCount,
+                    page: req.pagination.page,
+                    limit: req.pagination.limit,
+                },
+                success: true,
+                msg: "Vendors Fetched Successfully",
             })
 
         default:
