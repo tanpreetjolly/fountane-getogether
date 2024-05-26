cae3d28497ffc7358bc9abcf47c28d0ebccef485
tanpreetjolly <tanpreet2002@gmail.com>
2024-05-27 00:19:14 +0530

    refactor: update SubEventPage UI layout and add channel icons

--- client/src/Pages/SubEventPage.tsx
+++ client/src/Pages/SubEventPage.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react"
-import { FaPeopleCarry } from "react-icons/fa"
 import Button from "../components/Button"
-import { useNavigate } from "react-router-dom"
-import { BsFillPeopleFill } from "react-icons/bs"
+import { useNavigate, useParams } from "react-router-dom"
 import SwipeableDrawer from "@mui/material/SwipeableDrawer"
 import { AddCircleOutline } from "@mui/icons-material"
 import CreateChannelDrawer from "../components/CreateChannelDrawer"
+import { useEventContext } from "../context/EventContext"
+import Loader from "../components/Loader"
+import {
+  BookUser,
+  CalendarDays,
+  Group,
+  HandPlatter,
+  ListTodo,
+  MapPin,
+  Users,
+  Volume2,
+} from "lucide-react"
+import { SubEvent } from "../definitions"
+import { format } from "date-fns"
 
 const channelData = [
   {
@@ -20,17 +32,28 @@ const channelData = [
     id: "3",
     name: "Guest Only",
   },
+  {
+    id: "4",
+    name: "Photo Sharing",
+  },
 ]
 
-const lightShades = [
-  "bg-blue-600",
-  "bg-indigo-600",
-  "bg-cyan-600",
-  "bg-yellow-600",
-  "bg-purple-600",
-]
+const lightShades = ["bg-zinc-700", "bg-slate-700", "bg-dark", "bg-fuchsia-700"]
 
-const Channel = ({ name, colorIndex, id }: any) => {
+const getChannelIcon = (channelName: string) => {
+  switch (channelName) {
+    case "Announcements":
+      return <Volume2 size={18} />
+    case "Vendors Only":
+      return <HandPlatter size={18} />
+    case "Guest Only":
+      return <Users size={18} />
+    default:
+      return <Group size={18} />
+  }
+}
+
+const Channel = ({ name,id }: any) => {
   const navigate = useNavigate()
 
   return (
@@ -38,15 +61,19 @@ const Channel = ({ name, colorIndex, id }: any) => {
       onClick={() => {
         navigate(`channels/${id}`)
       }}
-      className={`text-gray-100 w-full text-left px-3 mb-3 py-2.5 text-lg font-medium ${lightShades[colorIndex]} rounded-lg`}
+      className={`text-slate-900  w-full text-left px-5 mb-2 py-3 border border-slate-300 shadow-sm rounded-xl flex items-center gap-2`}
     >
-      {name}
+      # {name}
+      {getChannelIcon(name)}
     </button>
   )
 }
-
 const SubEventChannels = () => {
   const navigate = useNavigate()
+  const formatDate = (date: string) => {
+    return format(new Date(date), "dd MMMM yyyy")
+  }
+  const { event, loadingEvent } = useEventContext()
   const [drawerOpen, setDrawerOpen] = useState<boolean>(false) // New state variable to track drawer type
   const toggleDrawer = (open: boolean) => (event: any) => {
     if (
@@ -59,35 +86,62 @@ const SubEventChannels = () => {
 
     setDrawerOpen(open)
   }
-  // const {id, subEventId} = useParams()
+  const { subEventId } = useParams()
+
+  const subEvent = event?.subEvents?.find(
+    (subEvent: SubEvent) => subEvent._id === subEventId,
+  )
+
+  if (loadingEvent) return <Loader />
 
   return (
-    <div className="px-4 flex flex-col justify-between h-full min-h-[90vh] pb-8">
-      <div>
-        <div className="flex justify-around gap-3 mb-1">
+    <div className="px-4 flex flex-col justify-between  h-[85vh] ">
+      <div className="">
+        {subEvent && (
+          <div className="flex flex-col items-start justify-between ">
+            <div className="px-1">
+              <div className="mb-1  text-2xl font-bold text-gray-800">
+                {subEvent.name}
+              </div>
+              <div className="text-sm text-indigo-600 font-medium rounded-full flex items-center gap-1 mb-1 ">
+                <CalendarDays className="inline mb-0.5" size={18} />
+                {formatDate(subEvent.startDate)} -{" "}
+                {formatDate(subEvent.endDate)}
+              </div>
+            </div>
+
+            <div className="text-base text-slate-800  rounded-full  flex items-center gap-1 pl-1">
+            <MapPin className="inline gap-1" size={16} />
+              {subEvent.venue}
+            </div>
+          </div>
+        )}
+        <div className="flex justify-around gap-3 mb-1 mt-2 font-inter">
           <button
             onClick={() => navigate(`invite-guests`)}
-            className="flex items-center justify-around bg-teal-500 text-white rounded-md w-1/2 px-4 py-3 gap-4"
+            className="flex items-center justify-around  bg-indigo-500 text-white rounded-lg w-1/2 px-4 py-3 gap-2"
           >
             <div>
-              <div className="text-gray-100 ">Invite</div>
-              <div className="font-bold text-xl"> Guests</div>
+              <div className="text-gray-100 text-sm text-left">Invite</div>
+              <div className="font-semibold text-xl"> Guests</div>
             </div>
-            <BsFillPeopleFill className="text-3xl" />
+            <BookUser size={30} />
           </button>
           <button
             onClick={() => navigate(`assign-vendors`)}
-            className="flex items-center justify-around bg-indigo-500 text-white border rounded-md w-1/2 px-4 py-3 gap-4"
+            className="flex items-center justify-around  bg-zinc-800 text-white  rounded-lg w-1/2 px-4 py-3 gap-4"
           >
             <div>
-              <div className="text-gray-100 ">Assign</div>
-              <div className="font-bold text-xl"> Vendors</div>
+              <div className="text-zinc-100 text-left text-sm">Assign</div>
+              <div className="font-semibold text-xl"> Vendors</div>
             </div>
-            <FaPeopleCarry className="text-4xl" />
+            <ListTodo size={30} />
           </button>
         </div>
-        <div className="p-1 text-lg text-gray-600 mb-1">Text Channels</div>
-        <div className="flex flex-col">
+        <div className="mb-1.5 text-xl text-zinc-800 mt-4  pl-1 font-semibold">
+          Text Channels
+        </div>
+        <div className="flex flex-col ">
           {channelData?.map((channel, index) => (
             <Channel
               key={channel.id}
