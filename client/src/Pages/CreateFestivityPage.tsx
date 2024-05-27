084a8ad231c13a2990e6e2c0c3559f572eb40dbc
tanpreetjolly <tanpreet2002@gmail.com>
2024-05-27 14:07:16 +0530

    refactor: improve CreateFestivityPage UI and functionality

--- client/src/Pages/CreateFestivityPage.tsx
+++ client/src/Pages/CreateFestivityPage.tsx
@@ -1,50 +1,48 @@
 import "react-date-range/dist/styles.css" // main style file
 import "react-date-range/dist/theme/default.css" // theme css file
-import { TextField } from "@mui/material"
-import { styled } from "@mui/system"
+import { useState } from "react"
 import { FaRegCalendarPlus } from "react-icons/fa"
 import { useNavigate } from "react-router-dom"
-import DatePicker from "../components/DatePicker"
+import { DatePickerWithRange } from "@/components/ui/DatePickerWithRange"
 import Button from "../components/Button"
-
-const RoundedTextField = styled(TextField)(({}) => ({
-  "& .MuiOutlinedInput-root": {
-    borderRadius: 10,
-  },
-}))
-
-// const RoundedSelect = styled(Select)(() => ({
-//   "& .MuiOutlinedInput-root": {
-//     borderRadius: 10,
-//   },
-// }))
 
 const CreateFestivity = () => {
   const navigate = useNavigate()
-
+  const [festivityName, setFestivityName] = useState("")
+  const [venue, setVenue] = useState("")
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+  console.log(startDate, endDate)
   const handleCreateEvent = () => {
+    // Handle form submission or navigate to another page
     navigate("/events/1/festivities/100")
   }
+
   return (
-    <div className="px-4 mx-auto flex flex-col min-h-[90vh]">
-      <RoundedTextField
+    <div className="px-4 mx-auto flex flex-col min-h-[85vh] gap-3">
+      <div className="text-2xl font-bold text-zinc-800">Create Festivity</div>
+      <input
         id="festivity"
         name="festivity"
-        label="Your Festivity Name Here"
-        variant="outlined"
-        fullWidth
+        placeholder="Your Festivity Name Here"
+        className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none"
+        value={festivityName}
+        onChange={(e) => setFestivityName(e.target.value)}
       />
-
-      <RoundedTextField
+      <input
         id="venue"
         name="venue"
-        label="Event Venue"
-        variant="outlined"
-        fullWidth
-        className="!mt-4"
+        placeholder="Event Venue"
+        className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none"
+        value={venue}
+        onChange={(e) => setVenue(e.target.value)}
       />
-
-      <DatePicker />
+      <DatePickerWithRange
+        startDate={startDate}
+        endDate={endDate}
+        setStartDate={setStartDate as any}
+        setEndDate={setEndDate as any}
+      />
       <div className="mt-auto mb-4">
         <Button
           text="Create Event"
@@ -54,12 +52,13 @@ const CreateFestivity = () => {
       </div>
       <style>
         {`
-        .rdrCalendarWrapper{
-          font-size : 0.8rem;
-        }
-        .rdrDateDisplayWrapper{
-          background-color: transparent;
-        }`}
+          .rdrCalendarWrapper{
+            font-size : 0.8rem;
+          }
+          .rdrDateDisplayWrapper{
+            background-color: transparent;
+          }
+        `}
       </style>
     </div>
   )
