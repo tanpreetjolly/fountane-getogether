5cf5f485bc991b6b21b08796d242a9ccf31a9b17
tanpreetjolly <tanpreet2002@gmail.com>
2024-05-29 12:11:14 +0530

    change calendar tile dimension ton fit in mobile view

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -32,7 +32,7 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-11 font-normal text-[0.8rem]",
+          "text-muted-foreground rounded-md w-10 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: cn(
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
@@ -42,7 +42,7 @@ function Calendar({
         ),
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-11 w-11 p-0 font-normal aria-selected:opacity-100",
+          "h-10 w-10 p-0 font-normal aria-selected:opacity-100",
         ),
         day_range_start: "day-range-start",
         day_range_end: "day-range-end",
