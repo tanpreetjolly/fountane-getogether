86132db06f0f4f4a341db1560a8615a6a0d883c1
tanpreetjolly <tanpreet2002@gmail.com>
2024-05-29 23:50:05 +0530

    refactor ui in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft, Bell, MoveRightIcon } from "lucide-react"
+import { ArrowLeft, Bell, MailPlus, MoveRightIcon } from "lucide-react"
 import { useNavigate } from "react-router-dom"
 import {
   DropdownMenu,
@@ -66,61 +66,59 @@ const Navbar = () => {
   console.log(notifications)
 
   return (
-    <div>
-      <nav className="bg-white fixed w-full z-20 top-0 start-0 border-gray-200">
-        <DropdownMenu>
-          <div className="flex justify-between items-center mx-auto px-1 pr-4 pt-3">
-            <button
-              className="p-2 rounded-full w-fit hover:bg-gray-200 focus:outline-none"
-              onClick={() => {
-                navigate(-1)
-              }}
-            >
-              <ArrowLeft size={24} />
+    <nav className="bg-white border-b  pb-2 justify-between fixed w-screen z-20 top-0 start-0 border-gray-200">
+      <DropdownMenu>
+        <div className="flex justify-between items-center mx-auto px-1 pr-4 pt-3">
+          <button
+            className="p-2 rounded-full w-fit hover:bg-gray-200 focus:outline-none"
+            onClick={() => {
+              navigate(-1)
+            }}
+          >
+            <ArrowLeft size={24} />
+          </button>
+          <DropdownMenuTrigger className="w-fit">
+            <button className="p-2 rounded-full hover:bg-gray-200 focus:outline-none relative ">
+              <MailPlus size={22} />
+              {notifications.length > 0 && (
+                <span className="bg-orange-500 h-2.5 w-2.5 absolute top-2 right-2 rounded-full"></span>
+              )}
             </button>
-            <DropdownMenuTrigger className="w-fit">
-              <button className="p-2 rounded-full hover:bg-gray-200 focus:outline-none relative ">
-                <Bell size={24} />
-                {notifications.length > 0 && (
-                  <span className="bg-orange-500 h-2.5 w-2.5 absolute top-2 right-2 rounded-full"></span>
-                )}
-              </button>
-            </DropdownMenuTrigger>
-          </div>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>Notifications</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            {notifications.length === 0 && (
-              <DropdownMenuItem>
-                <div className="w-40 italic text-gray-600">
-                  No new notifications
+          </DropdownMenuTrigger>
+        </div>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {notifications.length === 0 && (
+            <DropdownMenuItem>
+              <div className="w-40 italic text-gray-600">
+                No new notifications
+              </div>
+            </DropdownMenuItem>
+          )}
+          {notifications.map((notification) => (
+            <>
+              <DropdownMenuItem key={notification.id}>
+                <div
+                  onClick={() => navigate(`/invites`)}
+                  role="button"
+                  className="w-48 italic text-gray-600"
+                >
+                  You have an invitation from{" "}
+                  <span className="text-orange-500 font-medium">
+                    {notification.host} for {notification.eventName}
+                  </span>{" "}
+                  <span className="inline">
+                    <MoveRightIcon size={12} className="inline" />
+                  </span>
                 </div>
               </DropdownMenuItem>
-            )}
-            {notifications.map((notification) => (
-              <>
-                <DropdownMenuItem key={notification.id}>
-                  <div
-                    onClick={() => navigate(`/invites`)}
-                    role="button"
-                    className="w-48 italic text-gray-600"
-                  >
-                    You have an invitation from{" "}
-                    <span className="text-orange-500 font-medium">
-                      {notification.host} for {notification.eventName}
-                    </span>{" "}
-                    <span className="inline">
-                      <MoveRightIcon size={12} className="inline" />
-                    </span>
-                  </div>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-              </>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </nav>
-    </div>
+              <DropdownMenuSeparator />
+            </>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
   )
 }
 
