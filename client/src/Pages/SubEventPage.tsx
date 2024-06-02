4ba0a7d73b7412a8ac2d74161e6390a530b44137
aslezar <97354675+aslezar@users.noreply.github.com>
2024-06-02 22:25:10 +0530

    feat: Add user authentication check in SubEventChannels

--- client/src/Pages/SubEventPage.tsx
+++ client/src/Pages/SubEventPage.tsx
@@ -18,6 +18,7 @@ import {
 } from "lucide-react"
 import { ChannelType } from "../definitions"
 import { format } from "date-fns"
+import { useAppSelector } from "@/hooks"
 
 const getChannelIcon = (channelName: string) => {
   switch (channelName) {
@@ -53,6 +54,7 @@ const formatDate = (date: string) => {
 const SubEventChannels = () => {
   const [drawerOpen, setDrawerOpen] = useState<boolean>(false) // New state variable to track drawer type
   const { event, loadingEvent } = useEventContext()
+  const { user } = useAppSelector((state) => state.user)
 
   const navigate = useNavigate()
   const { subEventId } = useParams()
@@ -76,6 +78,15 @@ const SubEventChannels = () => {
   if (loadingEvent) return <Loader />
   if (!event) return <div>Event Not Found</div>
   if (!subEvent) return <div>Sub Event Not Found</div>
+  if (!user) return <div>User Not Found</div>
+
+  const isHost = user.userId === event.host._id
+  const isGuest = event.userList.some((guest) => guest.user._id === user.userId)
+  const isVendor = event.serviceList.some(
+    (service) => service.vendorProfile.user._id === user.userId,
+  )
+
+  console.log({ isHost, isGuest, isVendor })
 
   return (
     <div className="px-4 flex flex-col justify-between  h-[85vh] lg:w-4/5 mx-auto py-2">
@@ -96,35 +107,46 @@ const SubEventChannels = () => {
             {subEvent.venue}
           </div>
         </div>
-        <div className="flex justify-around gap-3 mb-1 mt-2 font-inter md:w-1/3">
-          <button
-            onClick={() => navigate(`guests`)}
-            className="flex items-center justify-around  bg-indigo-500 text-white rounded-lg w-1/2 px-4 py-3 gap-2"
-          >
-            <div>
-              <div className="text-gray-100 text-sm text-left">Invite</div>
-              <div className="font-semibold text-xl"> Guests</div>
-            </div>
-            <BookUser size={30} />
-          </button>
-          <button
-            onClick={() => navigate(`vendors`)}
-            className="flex items-center justify-around  bg-zinc-800 text-white  rounded-lg w-1/2 px-4 py-3 gap-4"
-          >
-            <div>
-              <div className="text-zinc-100 text-left text-sm">Assign</div>
-              <div className="font-semibold text-xl"> Vendors</div>
-            </div>
-            <ListTodo size={30} />
-          </button>
-        </div>
+        {isHost && (
+          <div className="flex justify-around gap-3 mb-1 mt-2 font-inter md:w-1/3">
+            <button
+              onClick={() => navigate(`guests`)}
+              className="flex items-center justify-around  bg-indigo-500 text-white rounded-lg w-1/2 px-4 py-3 gap-2"
+            >
+              <div>
+                <div className="text-gray-100 text-sm text-left">Invite</div>
+                <div className="font-semibold text-xl"> Guests</div>
+              </div>
+              <BookUser size={30} />
+            </button>
+            <button
+              onClick={() => navigate(`vendors`)}
+              className="flex items-center justify-around  bg-zinc-800 text-white  rounded-lg w-1/2 px-4 py-3 gap-4"
+            >
+              <div>
+                <div className="text-zinc-100 text-left text-sm">Assign</div>
+                <div className="font-semibold text-xl"> Vendors</div>
+              </div>
+              <ListTodo size={30} />
+            </button>
+          </div>
+        )}
         <div className="mb-1.5 text-xl text-zinc-800 mt-4  pl-1 font-semibold">
           Text Channels
         </div>
         <div className="flex flex-col ">
-          {subEvent.channels.map((channel) => (
-            <Channel key={channel._id} channel={channel} />
-          ))}
+          {subEvent.channels
+            .filter((channel) => {
+              if (isHost) return true
+              if (channel.name.toLowerCase() === "vendors only" && !isVendor)
+                return false
+              if (channel.name.toLowerCase() === "guests only" && !isGuest)
+                return false
+              return true
+            })
+            .map((channel) => (
+              <Channel key={channel._id} channel={channel} />
+            ))}
         </div>
       </div>
       <div className="flex justify-center gap-2 items-center fixed w-full backdrop-blur-md  py-4 px-4 left-1/2 translate-x-[-50%] bottom-14">
