46958ce9c771dd4edc8ea100ab30c2b1d9378cdf
tanpreetjolly <tanpreet2002@gmail.com>
2024-06-28 14:43:11 +0530

    add fontSize prop in button component to control font size

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -5,6 +5,7 @@ type Props = {
   icon?: any
   prim?: boolean
   wfull?: boolean
+  fontSize?: string
 }
 
 const Button = (props: Props) => {
@@ -12,10 +13,10 @@ const Button = (props: Props) => {
     <button
       disabled={props.disabled}
       onClick={props.onClick}
-      className={`flex items-center gap-2 justify-center ${props.wfull && "w-full"} max-w-md bg-dark bg-opacity-90  text-white py-2.5 hover:bg-opacity-100 px-6  rounded-xl`}
+      className={`flex items-center gap-1.5 justify-center ${props.wfull && "w-full"} max-w-md bg-dark bg-opacity-90  text-white py-2.5 hover:bg-opacity-100 px-6  rounded-xl`}
     >
       <span>{props?.icon}</span>
-      <span>{props.text}</span>
+      <span className={`${props.fontSize? props.fontSize: ""} `}>{props.text}</span>
     </button>
   )
 }
